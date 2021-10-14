ff87cff512517e4339e895c30713a1a5d1602d75
cedric ettori <ettori.cedric@gmail.com>
2021-10-14 15:38:53 +0200

    premier test api

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,8 @@
 require 'uri'
 require 'net/http'
 require 'openssl'
-require 'json'
 
-url = URI("https://streaming-availability.p.rapidapi.com/get/basic?country=fr&imdb_id=tt3799232&output_language=en")
+url = URI("https://streaming-availability.p.rapidapi.com/search/basic?country=fr&service=prime&type=movie&page=2&output_language=en&language=en")
 
 http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
@@ -19,21 +18,19 @@ http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 request = Net::HTTP::Get.new(url)
 request["x-rapidapi-host"] = 'streaming-availability.p.rapidapi.com'
-request["x-rapidapi-key"] = '1aef7e6fabmsh76ada75c37ba36ap1fcf99jsn647aa68d0153'
+request["x-rapidapi-key"] = '47e3704f9amsh0a64d5957b15463p1e1066jsn0d00a9711106'
 
-response = http.request(request) 
+response = http.request(request)
 data = JSON.parse(response.read_body)
-puts "#{data['title']}"
-puts "il est dispo sur : #{data['streamingInfo'].keys.join}"
-puts "Sortie en : #{data['year']}"
-puts data['posterURLs']['780']
+data['results'].each do |film|
+  puts film['title']
+  puts film['streamingInfo']
+end
 
-puts "creation d'un film"
-  movie = Movie.new(
-    name: data['title'],
-    year: data['year'],
-    url: data['posterURLs']['780']
-  )
-  movie.save!
-
+#puts "creation d'un film"
+#  movie = Movie.new(
+#    name: data['title'],
+#    year: data['year'],
+#    url: data['posterURLs']['780']
+#  )
 puts "creation ok" 
